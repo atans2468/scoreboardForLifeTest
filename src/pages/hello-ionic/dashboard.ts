06ab031435a01cde05a6d8550969b9e7e60cd95a
atans2468 <austintans7@gmail.com>
2017-07-31 21:18:45 -0700

    removing popover code

--- src/pages/hello-ionic/dashboard.ts
+++ src/pages/hello-ionic/dashboard.ts
@@ -1,10 +1,12 @@
 import { NavController, MenuController } from 'ionic-angular/index';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'dashboard.html'
 })
+
 export class Dashboard {
   constructor(private menu: MenuController, private nav: NavController, public navCtrl: NavController) { }
 
@@ -12,10 +14,5 @@ export class Dashboard {
   ionViewDidEnter() {
     this.menu.swipeEnable(true, 'menu1');
   }
-
-  /*displayDate(){
-    new Date().toLocaleDateString();
-  }
-  alert(displayDate);*/
 
 }
